fae39d421dd8ee7e3cf501fa749a0fe1aeddd155
Archita bansal <architabansall05@gmail.com>
2024-09-15 14:21:43 +0530

    forgot_password_view

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import 'app/routes/app_pages.dart';
+import 'app/modules/password_view/views/otp_view.dart';
+import 'app/modules/password_view/bindings/password_view_binding.dart';
+import 'app/modules/password_view/views/password_view_view.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp(PasswordViewApp());
+}
+
+class PasswordViewApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/otp',
+      getPages: [
+        GetPage(
+          name: '/otp',
+          page: () => OTPView(),
+          binding: PasswordViewBinding(),
+        ),
+        GetPage(
+          name: '/changePassword',
+          page: () => ChangePasswordView(),
+          binding: PasswordViewBinding(),
+        ),
+      ],
+    );
+  }
 }
