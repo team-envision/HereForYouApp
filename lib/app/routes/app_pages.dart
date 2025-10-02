3e47a2ed018e5dd49a80742797aa3ec4e0088022
Pranjal Garg <pranjalgarg2005@gmail.com>
2025-10-02 12:23:27 +0530

    fix: inital page set to getStarted

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -43,7 +43,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.LOGIN;
+  static const INITIAL = Routes.GET_STARTED;
 
   static final routes = [
     GetPage(
