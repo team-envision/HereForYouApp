9c4093dfa6a189ef76610370b9f499cb52238db1
Pranjal Garg <pranjalgarg2005@gmail.com>
2025-11-10 18:22:28 +0530

    fix: release build

--- lib/Components/featureCards.dart
+++ lib/Components/featureCards.dart
@@ -1,28 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:here_for_you_app/resources/app_resources/app_colors.dart';
 
-Widget kContainer(
-    {required Color color, required String text, required Widget icon,  dynamic? onTap}) {
+Widget kContainer({
+  required Color color,
+  required String text,
+  required Widget icon,
+  dynamic? onTap,
+  String? subText,
+}) {
   return GestureDetector(
     onTap: onTap,
     child: Container(
       margin: EdgeInsets.symmetric(horizontal: 5),
-      decoration:
-      BoxDecoration(color: color, borderRadius: BorderRadius.circular(22)),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(23.r),
+      ),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
+            padding: EdgeInsets.symmetric(horizontal: 15.w),
             child: Text(
               text,
-              style: Get.theme.textTheme.titleLarge
-                  ?.copyWith(color: AppColors.white, fontWeight: FontWeight.bold),
+              style: GoogleFonts.urbanist(
+                fontWeight: FontWeight.w800,
+                fontSize: 22.29.sp,
+                color: AppColors.white,
+                height: 24 / (22.29.sp),
+              ),
             ),
           ),
-          icon
+          icon,
         ],
       ),
     ),
--- lib/app/modules/ProfilePage/views/profile_page_view.dart
+++ lib/app/modules/ProfilePage/views/profile_page_view.dart
@@ -2,15 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:here_for_you_app/Components/customPopup.dart';
 import 'package:here_for_you_app/Components/featureContainers.dart';
 import 'package:here_for_you_app/Components/kElevatedButton.dart';
 import 'package:here_for_you_app/Components/popUpMenu.dart';
 import 'package:here_for_you_app/app/routes/app_pages.dart';
 import 'package:here_for_you_app/resources/app_resources/app_colors.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 import '../../../../Components/featureCards.dart';
 import '../../../views/views/MindAnchorView.dart';
+import '../../SleepDiary/controllers/sleep_diary_controller.dart';
+import '../../SleepDiary/views/sleepDiaryHomeView.dart';
+import '../../SleepDiary/views/sleep_diary_view.dart';
 import '../../editProfileView/views/edit_profile_view.dart';
 import '../../mentalScore/views/mental_score_view.dart';
 import '../../moodQuality/views/mood_quality_view.dart';
@@ -140,46 +145,70 @@ class ProfilePageView extends GetView<ProfilePageController> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 200,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: ListView(
-                        itemExtent: 170,
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          kContainer(
-                            color: AppColors.mentalScoreCard,
-                            text: "Mental\nScore",
-                            onTap: () {
-                              Get.to(() => const MentalScoreView());
-                            },
-                            icon: SvgPicture.asset(
-                              "assets/icons/mentalScoreIcon.svg",
-                            ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10.w),
+                    height: 171.h,
+                    child: ListView(
+                      itemExtent: 170,
+                      scrollDirection: Axis.horizontal,
+                      physics: const BouncingScrollPhysics(),
+                      children: [
+                        kContainer(
+                          color: AppColors.mentalScoreCard,
+                          text: "Mental\nscore",
+                          onTap: () {
+                            Get.to(() => const MentalScoreView());
+                          },
+                          icon: SvgPicture.asset(
+                            "assets/icons/mentalScoreIcon.svg",
                           ),
-                          kContainer(
-                            color: AppColors.mindAnchorCard,
-                            text: "Mind\nAnchor",
-                            onTap: () {
-                              Get.to(() => const Mindanchorview());
-                            },
-                            icon: SvgPicture.asset(
-                              "assets/icons/AnchorIcon.svg",
-                            ),
+                        ),
+                        kContainer(
+                          color: AppColors.mindAnchorCard,
+                          text: "Mind\nAnchor",
+                          onTap: () {
+                            Get.to(() => const Mindanchorview());
+                          },
+                          icon: SvgPicture.asset("assets/icons/AnchorIcon.svg"),
+                        ),
+                        kContainer(
+                          color: AppColors.moodQualityCard,
+                          onTap: () {
+                            Get.to(() => const MoodQualityView());
+                          },
+                          text: "Mood\nQuality",
+                          icon: SvgPicture.asset(
+                            "assets/icons/moodQulatiyIcon.svg",
                           ),
-                          kContainer(
-                            color: AppColors.moodQualityCard,
-                            text: "Mood\nQuality",
-                            onTap: () {
-                              Get.to(() => const MoodQualityView());
-                            },
-                            icon: SvgPicture.asset(
-                              "assets/icons/moodQulatiyIcon.svg",
-                            ),
+                        ),
+                        kContainer(
+                          color: AppColors.stressSecondary,
+                          onTap: () => Get.toNamed(Routes.STRESS_INDICATOR),
+                          text: "Stress\nLevel",
+                          icon: stressLevel(),
+                        ),
+                        kContainer(
+                          color: AppColors.mentalScoreCard,
+
+                          onTap: () async {
+                            final sleepcontroller = Get.put(
+                              SleepDiaryController(),
+                            );
+                            bool hasSetReminder = await sleepcontroller
+                                .hasSetReminder();
+                            if (hasSetReminder) {
+                              Get.to(SleepDiaryHomeView());
+                            } else {
+                              Get.to(SleepDiaryView());
+                            }
+                          },
+
+                          text: "Sleep\nDiary",
+                          icon: SvgPicture.asset(
+                            "assets/icons/sleepdiaryicon.svg",
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                   SizedBox(
@@ -233,6 +262,90 @@ class ProfilePageView extends GetView<ProfilePageController> {
                 ],
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget stressLevel() {
+    return SizedBox(
+      width: 400.23.w,
+      height: 47.52.h,
+      child: SfRadialGauge(
+        axes: [
+          RadialAxis(
+            minimum: 0,
+            maximum: 100,
+            showLabels: false,
+            showTicks: false,
+            startAngle: 180,
+            endAngle: 360,
+            radiusFactor: 1.8,
+            canScaleToFit: true,
+            pointers: [
+              MarkerPointer(
+                value: 20,
+                markerHeight: 15.47.h,
+                markerWidth: 15.47.w,
+                color: AppColors.white,
+                markerType: MarkerType.circle,
+              ),
+            ],
+            ranges: [
+              GaugeRange(
+                startValue: 0,
+                endValue: 20,
+                color: Color(0xFFE0FFA5),
+                startWidth: 10,
+                // Add this
+                endWidth: 10, // Add this
+              ),
+              GaugeRange(
+                startValue: 20,
+                endValue: 40,
+                color: Color(0xFFC7EA85),
+                startWidth: 10,
+                // Add this
+                endWidth: 10, // Add this
+              ),
+              GaugeRange(
+                startValue: 40,
+                endValue: 60,
+                color: Color(0x214E7309),
+                startWidth: 10,
+                // Add this
+                endWidth: 10, // Add this
+              ),
+              GaugeRange(
+                startValue: 60,
+                endValue: 80,
+                color: Color(0x524E7309),
+                startWidth: 10,
+                // Add this
+                endWidth: 10, // Add this
+              ),
+              GaugeRange(
+                startValue: 80,
+                endValue: 100,
+                color: Color(0xFF4E7309),
+                startWidth: 10,
+                // Add this
+                endWidth: 10, // Add this
+              ),
+            ],
+            annotations: [
+              GaugeAnnotation(
+                positionFactor: 0,
+                widget: Text(
+                  "20%",
+                  style: GoogleFonts.urbanist(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 17.32.sp,
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
